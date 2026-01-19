07826e9600e23cb46acca119162401c5c0f90776
Fabián Huertas <miloh9318@hotmail.com>
2026-01-18 19:40:17 -0500

    primeras variables en js

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+    let inputSearch = document.querySelector(".search__input");
+    let btnSearch = document.querySelector(".navbar__button");
+
+    btnSearch.addEventListener("click", () => {
+        
+    });
+});
