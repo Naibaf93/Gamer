39f56847a618c04f34465a36bbbf8768d09c2db4
Fabián Huertas <miloh9318@hotmail.com>
2026-01-23 14:50:22 -0500

    creando primeras variables en menu.js

---
+++ assets/js/menu.js
@@ -0,0 +1,4 @@
+document.activeElement("DOMContentLoaded", (e) => {
+    let mobileBtn = document.querySelector(".navbar__mobile-btn");
+    let mobileMenu = document.querySelector(".navbar__mobile-list");
+})
