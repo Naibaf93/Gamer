dc238e8dff675fe3f00df978cc135ed1708c7b88
Fabián Huertas <miloh9318@hotmail.com>
2026-01-23 14:54:43 -0500

    js menu responsive parte 1

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,16 @@
 document.activeElement("DOMContentLoaded", (e) => {
     let mobileBtn = document.querySelector(".navbar__mobile-btn");
     let mobileMenu = document.querySelector(".navbar__mobile-list");
-})
+
+    mobileBtn.addEventListener("click", () => {
+        let menuOpen = document.querySelector(".menu__open");
+
+        if(!menu__open) {
+            mobileMenu.style.display = "block";
+            mobileMenu.classList.add("menu__open");
+        }else{
+            mobileMenu.style.display = "none";
+            mobileMenu.classList.remove("menu__open");
+        }
+    });
+});
